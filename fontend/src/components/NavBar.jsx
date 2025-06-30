9cae44241bed9eb42c9bacdd192938a1b1ee9a84
stoyj <stoyanka.kamenova@gmail.com>
2025-06-30 10:20:54 +0300

    comments

--- fontend/src/components/NavBar.jsx
+++ fontend/src/components/NavBar.jsx
@@ -1,12 +1,20 @@
+//➡️ Link е като бутон, но изглежда като текстов линк, който не презарежда страницата.
+//С него минаваме между страници много бързо, без да чакаме.
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
+//➡️ Внасяме стиловете – за да изглежда менюто красиво (цвят, разположение и т.н.)
 
+//➡️ Това е нашето меню.
+//➡️ Създаваме <nav> – това е HTML елемент за навигация (меню).
 function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/">Movie App</Link>
+        {/*➡️ Показваме заглавието „Movie App“, и ако го цъкнеш – те връща на началната страница (/). */}
       </div>
+
+      {/* ➡️ Това са два бутона/връзки: */}
       <div className="navbar-links">
         <Link to="/" className="nav-link">
           Home
@@ -20,3 +28,4 @@ function NavBar() {
 }
 
 export default NavBar;
+//➡️ Казваме: това меню вече може да се използва в други файлове.
